ec75878e0417f0dc70a11442807d9f83654a3ca9
dhacker29 <dhackerdvm@gmail.com>
2014-01-28 23:42:11 -0600

    Gummify
    Change-Id: I725ef5336617f35c2ad17169e9c2eaa397be0581

--- tg.mk
+++ tg.mk
@@ -1,8 +1,8 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Gummy stuff.
+$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -10,7 +10,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := MOTO X
-PRODUCT_NAME := cm_ghost
+PRODUCT_NAME := tg_ghost
 
 $(call inherit-product, device/motorola/ghost/full_ghost.mk)
 
